c771565d5cc3823525197585e421e004722d8188
nicolas <nicolascapellino05@gmail.com>
2025-04-06 22:39:07 -0300

    feat: implement Monoid and Semigroup instances for Mem type

--- tarea-dos/monoid-exercise.hs
+++ tarea-dos/monoid-exercise.hs
@@ -13,10 +13,24 @@ import Data.Monoid
 newtype Mem s a =
     Mem { runMem :: s -> (a, s) }
 
-instance Monoid a => Monoid (Mem s a) where
-    mempty = -- TO DO
-    mappend = -- TO DO
 
+-- Originalemnte lo hice de esta forma, pero me saltaba error
+-- porque aparentemente en versiones nuevas de Hasckell no lo permite
+--instance Monoid a => Monoid (Mem s a) where
+--    mempty = Mem $ \s -> (mempty, s) 
+--    mappend (Mem f) (Mem g) = Mem $ \s ->
+--        let (a1, s1) = f s 
+--            (a2, s2) = g s1 
+--        in (a1 <> a2, s2)
+
+instance Semigroup a => Semigroup (Mem s a) where
+    (Mem f) <> (Mem g) = Mem $ \s ->
+        let (a1, s1) = f s
+            (a2, s2) = g s1
+        in (a1 <> a2, s2)
+
+instance Monoid a => Monoid (Mem s a) where
+    mempty = Mem $ \s -> (mempty, s) 
 
 
 f' = Mem $ \s -> ("hi", s + 1)
