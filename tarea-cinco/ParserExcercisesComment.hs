dcd4743adc469c4b5be039233c22f49cd07f3f09
Bruno Monfort <brunomonfort07@gmail.com>
2025-05-14 20:23:55 -0300

    Version final. Entregar ParserExercises.hs

--- tarea-cinco/ParserExcercisesComment.hs
+++ tarea-cinco/ParserExcercisesComment.hs
@@ -16,19 +16,39 @@ record ::= { identifier : type ( , field : type )* }
 type ::= bool | int | string
 -}
 
+-- symbol sale de Parsing.hs sirve para reconocer una cadena de texto determinada ignorando los espacios en blanco alrededor. Es una versión "espaciada" del parser string.
+
+
+-- oneField  toma un campo como 'field1: bool' y devuelve '("field1", "bool")'.
+-- Este parser:
+--      Espera un nombre de campo (identifier)
+--      Luego el símbolo :
+--      Luego uno de los tipos permitidos (bool, int, string)
+--      Devuelve una tupla con el nombre del campo y el tipo
 oneField :: Parser (String, String)
 oneField = do
     fieldName <- identifier
     _ <- symbol ":"
     aType <- symbol "bool" <|> symbol "int" <|> symbol "string"
     return (fieldName, aType)
-    
+
+-- fields extiende el uso de oneField a varios fields y los separa por coma
+-- Este parser:
+--      Espera al menos un oneField
+--      Luego cero o más campos adicionales precedidos por ,
+--      Junta todos los campos en una lista
 fields :: Parser [(String, String)]
 fields = do
     f  <- oneField
     fs <- many (do _ <- symbol ","; oneField)         
     return (f : fs)
 
+-- record recibe el string entre llaves y devuelve la lista de tuplas con el nombre del field y el tipo asociado [(String,String)]
+-- Este parser:
+--      Espera una llave izquierda {
+--      Luego los campos
+--      Luego una llave derecha }
+--      Devuelve la lista de campos
 record :: Parser [(String, String)]
 record = do
     _ <- symbol "{"
@@ -79,16 +99,28 @@ factor = do symbol "("
          <|> integer
 
 
-parseTest1 = parse expr "2 + 3 * 4"
+parseTest4 = parse expr "2 + 3 * 4"
+-- Esperado: [(14,"")]
 
-parseTest2 = parse expr "10 - 2"
+-- Resta
+parseTest6 = parse expr "10 - 2"
+-- Esperado: [(8,"")]
 
-parseTest3 = parse expr "20 / 5"
+-- División con números enteros
+parseTest7 = parse expr "20 / 5"
+-- Esperado: [(4,"")]
 
-parseTest4 = parse expr "8 / 0" -- Lo interpretamos como "Si falla, no le hagas 'parse'."
+-- División por cero
+parseTest8 = parse expr "8 / 0"
+-- Esperado: [] (falla silenciosamente)
 
-parseTest5 = parse expr "1 + 2 * (3 + 4) - 5"
+-- Expresión compleja con múltiples operaciones
+parseTest9 = parse expr "1 + 2 * (3 + 4) - 5"
+-- Esperado: [(10,"")]
 
-parseTest6 = parse expr "-3 + 7"
+-- Negativos
+parseTest10 = parse expr "-3 + 7"
+-- Esperado: [(4,"")]
 
-parseTest7 = parse expr "1 + 2 * (2 + 6) / 4"
+-- Expresión compleja con múltiples operaciones
+parseTest11 = parse expr "1 + 2 * (2 + 6) / 4"
